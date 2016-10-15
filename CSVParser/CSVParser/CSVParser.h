f3c6549e3d6f8d3d029ca2a834c5883c16929717
agarzonp <agarzonp@gmail.com>
2016-10-15 13:09:14 +0200

    Project created with empty CSVParser.h

---
+++ CSVParser/CSVParser/CSVParser.h
@@ -0,0 +1,21 @@
+#ifndef CSV_PARSER
+#define CSV_PARSER
+
+namespace agarzonp
+{
+	class CSVParser
+	{
+	public:
+
+		CSVParser(const char* file)
+		{
+
+		}
+
+		~CSVParser()
+		{
+		}
+	};
+}
+
+#endif // !CSV_PARSER
---
+++ CSVParser/main.cpp
@@ -0,0 +1,8 @@
+#include "CSVParser/CSVParser.h"
+
+int main(int argc, char **argv) 
+{
+	agarzonp::CSVParser csvParser("");
+
+	return 0;
+}
